cce4c223ff0f29ac50c638a7a907999de6791597
eniabellver <eugeniabellver@gmail.com>
2022-02-11 17:16:43 +0000

    react starter

---
+++ react-starter/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the Yummy Brummy React starter!</h1>
+      <p>
+        If you'd like to contribute to migrating this simple to a React app,
+        please let us know in the GitHub issues pannel.
+        <br />
+        The maintainance team will be there to help you, provide feedback and
+        assist you through the contributing process.
+        <br />
+        For more information read the main branch README.md
+      </p>
+    </div>
+  );
+}
+
+export default App;
